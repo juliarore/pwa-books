c7219d2f25e7beb747d42f5b5e602e3835048a1b
Júlia Romero <juliarore6@gmail.com>
2026-05-23 19:33:26 +0200

    feat: add reusable shared components

--- src/app/pages/books-list/books-list.component.ts
+++ src/app/pages/books-list/books-list.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+
+import { Book } from '../../models/book.model';
+import { BooksService } from '../../services/books.service';
 
 @Component({
   selector: 'app-books-list',
+  standalone: true,
   imports: [],
   templateUrl: './books-list.component.html',
-  styleUrl: './books-list.component.scss'
+  styleUrl: './books-list.component.scss',
 })
-export class BooksListComponent {
+export class BooksListComponent implements OnInit {
+  private booksService = inject(BooksService);
 
+  books = signal<Book[]>([]);
+
+  ngOnInit(): void {
+    this.booksService.getBooks().subscribe({
+      next: (books) => {
+        this.books.set(books);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 }
---
+++ src/app/shared/components/books-grid/books-grid.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books-grid',
+  imports: [],
+  templateUrl: './books-grid.component.html',
+  styleUrl: './books-grid.component.scss'
+})
+export class BooksGridComponent {
+
+}
