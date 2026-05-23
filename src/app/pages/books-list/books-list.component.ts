6d8d224ecf0e6659d1b83de7ac6a2e94f95bfa48
Júlia Romero <juliarore6@gmail.com>
2026-05-23 23:14:56 +0200

    feat: improve BooksList component with loading state and grid layout

--- src/app/pages/books-list/books-list.component.ts
+++ src/app/pages/books-list/books-list.component.ts
@@ -1,27 +1,33 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-
+import { Component, OnInit, signal } from '@angular/core';
 import { Book } from '../../models/book.model';
 import { BooksService } from '../../services/books.service';
+import { BookCardComponent } from '../../shared/components/book-card/book-card.component';
+import { LoadingSpinnerComponent } from '../../shared/components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-books-list',
   standalone: true,
-  imports: [],
+  imports: [BookCardComponent, LoadingSpinnerComponent],
   templateUrl: './books-list.component.html',
   styleUrl: './books-list.component.scss',
 })
 export class BooksListComponent implements OnInit {
-  private booksService = inject(BooksService);
-
   books = signal<Book[]>([]);
+  loading = signal(false);
+
+  constructor(private booksService: BooksService) {}
 
   ngOnInit(): void {
+    this.loading.set(true);
     this.booksService.getBooks().subscribe({
       next: (books) => {
         this.books.set(books);
+        this.loading.set(false);
       },
+
       error: (error) => {
         console.error(error);
+        this.loading.set(false);
       },
     });
   }
