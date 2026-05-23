5ba8203f196e0d3710f27cf5e72387905288269f
Júlia Romero <juliarore6@gmail.com>
2026-05-23 23:15:20 +0200

    feat: implement BookCard component with Material design and rating stars

--- src/app/shared/components/book-card/book-card.component.ts
+++ src/app/shared/components/book-card/book-card.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { RouterLink } from '@angular/router';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+import { Book } from '../../../models/book.model';
 
 @Component({
   selector: 'app-book-card',
-  imports: [],
+  standalone: true,
+  imports: [MatCardModule, MatIconModule, RouterLink],
   templateUrl: './book-card.component.html',
-  styleUrl: './book-card.component.scss'
+  styleUrl: './book-card.component.scss',
 })
 export class BookCardComponent {
-
+  @Input() book!: Book;
+  stars = Array(5);
 }
