69cd5aed570bb12f2e1254171c35288a0dd1d7f7
Júlia Romero <juliarore6@gmail.com>
2026-05-23 19:04:24 +0200

    feat: define application routes for books list and detail components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { BooksDetailComponent } from './pages/books-detail/books-detail.component';
+import { BooksListComponent } from './pages/books-list/books-list.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'books',
+    pathMatch: 'full',
+  },
+  {
+    path: 'books',
+    component: BooksListComponent,
+  },
+  {
+    path: 'books/:id',
+    component: BooksDetailComponent,
+  },
+];
