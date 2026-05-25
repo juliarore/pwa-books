b824c14fb92da884f24bdab2053ce3d976bf2f8d
Júlia Romero <juliarore6@gmail.com>
2026-05-25 13:45:36 +0200

    feat: implement BooksDetail component with loading state and book details display

--- src/app/pages/books-detail/books-detail.component.ts
+++ src/app/pages/books-detail/books-detail.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+
+import { Book } from '../../models/book.model';
+
+import { BooksService } from '../../services/books.service';
+
+import { LoadingSpinnerComponent } from '../../shared/components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-books-detail',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterLink,
+    MatButtonModule,
+    MatExpansionModule,
+    MatIconModule,
+    LoadingSpinnerComponent,
+  ],
   templateUrl: './books-detail.component.html',
-  styleUrl: './books-detail.component.scss'
+  styleUrl: './books-detail.component.scss',
 })
-export class BooksDetailComponent {
+export class BooksDetailComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private booksService: BooksService,
+  ) {}
 
+  book = signal<Book | null>(null);
+
+  loading = signal(true);
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.booksService.getBookById(id).subscribe({
+        next: (book) => {
+          this.book.set(book);
+
+          this.loading.set(false);
+        },
+
+        error: (err) => {
+          console.error(err);
+
+          this.loading.set(false);
+        },
+      });
+    } else {
+      this.loading.set(false);
+    }
+  }
 }
