5ca3d5626732dbed1e820c54c5bb1552b29752da
Júlia Romero <juliarore6@gmail.com>
2026-05-25 13:40:20 +0200

    feat: enhance BooksList component with improved layout and view mode toggle

--- src/app/pages/books-list/books-list.component.ts
+++ src/app/pages/books-list/books-list.component.ts
@@ -1,32 +1,53 @@
 import { Component, OnInit, signal } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 import { Book } from '../../models/book.model';
+
 import { BooksService } from '../../services/books.service';
+
 import { BookCardComponent } from '../../shared/components/book-card/book-card.component';
+import { BooksTableComponent } from '../../shared/components/books-table/books-table.component';
 import { LoadingSpinnerComponent } from '../../shared/components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-books-list',
   standalone: true,
-  imports: [BookCardComponent, LoadingSpinnerComponent],
+  imports: [
+    BookCardComponent,
+    LoadingSpinnerComponent,
+    MatButtonModule,
+    MatIconModule,
+    BooksTableComponent,
+  ],
   templateUrl: './books-list.component.html',
   styleUrl: './books-list.component.scss',
 })
 export class BooksListComponent implements OnInit {
   books = signal<Book[]>([]);
-  loading = signal(false);
+
+  loading = signal(true);
+
+  viewMode = signal<'cards' | 'table'>('cards');
+
+  toggleViewMode() {
+    this.viewMode.set(this.viewMode() === 'cards' ? 'table' : 'cards');
+  }
 
   constructor(private booksService: BooksService) {}
 
   ngOnInit(): void {
-    this.loading.set(true);
     this.booksService.getBooks().subscribe({
       next: (books) => {
         this.books.set(books);
+
         this.loading.set(false);
       },
 
       error: (error) => {
         console.error(error);
+
         this.loading.set(false);
       },
     });
